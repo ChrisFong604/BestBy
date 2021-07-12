3f63ff0f917dfe69799ee354cf35d3bdcf9207a1
ChrisFong604 <ccffoonngg@gmail.com>
2021-07-12 15:39:26 -0700

    removed components for now

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, TextInput, Button, Alert, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import DumbComponent from './components/DumbComponent';
 import NewView from './components/NewView';
 import styles from './stylesheets/Default';
