87367e0c174491fc53adc7ee400698854a8ba634
ChrisFong604 <ccffoonngg@gmail.com>
2021-07-13 15:08:41 -0700

    Firebase application connected to registration

--- App.js
+++ App.js
@@ -2,10 +2,28 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text } from "react-native";
 
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCeiTn_h_M0Qn142qjJt32-6tGSQixTgWw",
+	authDomain: "bestby-2a9ad.firebaseapp.com",
+	projectId: "bestby-2a9ad",
+	storageBucket: "bestby-2a9ad.appspot.com",
+	messagingSenderId: "201942630744",
+	appId: "1:201942630744:web:240677cca611bc2f0be52d",
+	measurementId: "G-VHHP35HT8D",
+};
+
+if (firebase.apps.length === 0) {
+	firebase.initializeApp(firebaseConfig);
+}
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LandingScreen from "./components/auth/Landing";
+import RegisterScreen from "./components/auth/Register";
+
 const Stack = createStackNavigator();
 
 export default function App() {
@@ -17,6 +35,7 @@ export default function App() {
 					component={LandingScreen}
 					options={{ headerShown: false }}
 				/>
+				<Stack.Screen name="Register" component={RegisterScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
