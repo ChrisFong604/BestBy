bab5b70b03069f0ef8cf4388f92d59ab6e0df0d6
ChrisFong604 <ccffoonngg@gmail.com>
2021-08-25 13:39:09 -0700

    added loading anim while fetching data

--- components/main/ingredients/IngredientsList.js
+++ components/main/ingredients/IngredientsList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, SafeAreaView, Button } from "react-native";
+import { View, SafeAreaView, ActivityIndicator } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import { QStext } from "../../UI-Components/QStext";
@@ -29,21 +29,7 @@ function IngredientsListScreen({ navigation }) {
 		fetchData();
 	}, []);
 
-	if (loading) {
-		return (
-			<View
-				style={{
-					flex: 1,
-					justifyContent: "center",
-					flexDirection: "row",
-					alignItems: "center",
-				}}
-			>
-				<QStext text={"Loading"} h2 />
-			</View>
-		);
-	}
-	return (
+	return loading ? (
 		<SafeAreaView style={Default.ViewContainer}>
 			<View style={{ flexDirection: "row" }}>
 				<QStext text={"Current Inventory"} h2 />
@@ -65,6 +51,36 @@ function IngredientsListScreen({ navigation }) {
 			>
 				<QStext text={"Add Ingredient"} p />
 			</MaterialCommunityIcons.Button>
+			<ActivityIndicator size="large" color="blue" />
+		</SafeAreaView>
+	) : (
+		<SafeAreaView style={Default.ViewContainer}>
+			<View style={{ flexDirection: "row" }}>
+				<QStext text={"Current Inventory"} h2 />
+			</View>
+
+			<MaterialCommunityIcons.Button
+				name="plus-circle"
+				color={"black"}
+				iconStyle={{
+					margin: 0,
+					padding: 0,
+					alignItems: "center",
+					justifyContent: "center",
+				}}
+				backgroundColor={"orange"}
+				size={20}
+				borderRadius={100}
+				onPress={() => navigation.navigate("AddIngredient")}
+			>
+				<QStext text={"Add Ingredient"} p />
+			</MaterialCommunityIcons.Button>
+
+			{() => {
+				if (loading) {
+					return <ActivityIndicator size="large" color="#0000ff" />;
+				}
+			}}
 
 			{foodInventory.map((food, index) => (
 				<Ingredient key={index} Ingredient={food} />
