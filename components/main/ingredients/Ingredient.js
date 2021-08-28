ef8c0884f3cad0de2ee827c47aa94a9c148b605f
Kevin <kevins.shi2001@gmail.com>
2021-08-28 10:37:30 -0600

    stash

--- components/main/ingredients/Ingredient.js
+++ components/main/ingredients/Ingredient.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+
 import { Text, View, Button } from "react-native";
 import { QStext } from "../../UI-Components/QStext";
+import React, { useState, useEffect, useFocusEffect } from "react";
 
 const Ingredient = ({ Ingredient }) => {
+	const [date, setDate] = useState('');
+
+	useEffect(() => {
+		var day = new Date().getDate();
+		var month = new Date().getMonth();
+		var year = new Date().getFullYear();
+		setDate(day + '/' + month + '/' + year);
+	}, [])
+
 	return (
 		<>
 			<Text>{Ingredient.name}</Text>
 			<Text>{Ingredient.foodgroup}</Text>
+			<Text>{date}</Text>
 		</>
 	);
 };
